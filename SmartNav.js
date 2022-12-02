9f8b1323941e1e22a1b02542402a91b1fa6ff9ae
zaixu <zaixu91@gmail.com>
2022-12-02 23:17:59 +0900

    개발중

--- SmartNav.js
+++ SmartNav.js
@@ -10,6 +10,11 @@ class SmartNav{
 		this.nav = nav;
 		this.navList = Array.prototype.slice.call(document.querySelector(nav).querySelectorAll('a'));
 		this.navCont = [];
+		this.duration = 300;
+
+		if(options){
+			this.duration = typeof options.duration != 'undefined' ? options.duration : this.duration;
+		}
 
 		this.initialize();
 	}
@@ -17,69 +22,61 @@ class SmartNav{
 	initialize(){
 		this.navList.forEach(ele => {
 			this.navCont.push(ele.hash);
+			const duration = this.duration;
 
 			ele.addEventListener('click', function(e){
 				e.preventDefault();
 
 				let navCont = document.getElementById(ele.hash.replace('#', ''));
-				let currentTop = window.scrollY;
-				let winTop = currentTop + navCont.getBoundingClientRect().top;
+				let scrollTop = {};
+				scrollTop.from = window.scrollY;
+				scrollTop.to = scrollTop.from + navCont.getBoundingClientRect().top;
 
-				/* window.scroll({
-					left: 0,
-					top: 1000,
-					behavior: 'smooth'
-				}); */
-
-				/* let num = 0;
-				let numInterval = setInterval(() => {
-					num++;
-					console.log('timer:', num);
-				}, 1000); */
-				
-				/* let myInterval = setInterval(() => {
-					console.log('myInterval');
-					window.scrollTo(0, ++currentTop);
-					console.log(currentTop);
-
-					if(currentTop >= winTop){
-						console.log('clearInterval');
-						clearInterval(myInterval);
-						clearInterval(numInterval);
-						console.log(num);
-					}
-				}, 10); */
-
-				function scrollToSmoothly(pos, time) {
-					var currentPos = window.scrollY;
-					var start = null;
-					if(time == null) time = 500;
-					pos = +pos,time = +time;
-					window.requestAnimationFrame(function step(currentTime) {
-					    start = !start ? currentTime : start;
-					    var progress = currentTime - start;
-					    if (currentPos < pos) {
-						  window.scrollTo(0, ((pos - currentPos) * progress / time) + currentPos);
-					    } else {
-						  window.scrollTo(0, currentPos - ((currentPos - pos) * progress / time));
-					    }
-					    if (progress < time) {
-						  window.requestAnimationFrame(step);
-					    } else {
-						  window.scrollTo(0, pos);
-					    }
+				let start = new Date().getTime();
+				let timer = setInterval(() => {
+					var time = new Date().getTime() - start;
+					var x = easeInQuad(time, scrollTop.from, scrollTop.to - scrollTop.from, duration);
+					window.scrollTo({
+						left: 0,
+						top: x
 					});
-				  }
+					if(time >= duration){
+						clearInterval(timer);
+						window.scrollTo({
+							left: 0,
+							top: scrollTop.to
+						});
+					}
+				}, 1000 / 60);
 
-				  scrollToSmoothly(winTop, 250);
+				//http://easings.net
+				//https://github.com/bameyrick/js-easing-functions/blob/master/src/index.ts
+				//t: current time
+				//b: beginning value(from)
+				//c: change in value(to)
+				//d: duration
+				function linear(t, b, c, d) {
+					return c * t / d + b;
+				}
+
+				function easeInQuad(t, b, c, d){
+					return c * (t /= d) * t + b;
+				}
+
+				/*
+				function easeInQuart(t, b, c, d){
+					return c * (t /= d) * t * t * t + b;
+				}
+
+				function easeInOutQuart(t, b, c, d){
+					if ((t /= d / 2) < 1) {
+						return c / 2 * t * t * t * t + b;
+					}else{
+						return -c / 2 * ((t -= 2) * t * t * t - 2) + b;
+					}
+				}
+				*/
 			});
 		});
 	}
 }
-
-/*
-	10px을 1000ms에 이동하려면?
-		=> 100ms마다 1px씩 이동
-	1000px을 500ms에 이동하려면?
-		=> 0.5ms마다 1px씩 이동
-*/
