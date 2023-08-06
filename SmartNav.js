7dfaf6a64cb3bde86a92bac14fd976d385fac420
zaixu <zaixu91@gmail.com>
2023-08-06 17:05:31 +0900

    개발중

--- SmartNav.js
+++ SmartNav.js
@@ -8,20 +8,21 @@
 class SmartNav{
 	constructor(selector, options){
 		this.wrapper = selector;
-		this.navBtns = document.querySelector(selector).querySelectorAll('a');
-		this.duration = options?.duration ?? 500;
-		this.fixed = options?.fixed ?? 0;
-		this.transPoint = options?.transPoint ? options.transPoint * window.innerHeight : 0;
-		this.activeClass = options?.activeClass;
-		this.easing = options?.easing ?? 'ease';
-		this.iosNotch = options?.iosNotch ?? this.notchValue;
-		this.#init();
+		this.navBtns = document.querySelector(selector).querySelectorAll('a[href^="#"]');
 
-		window.addEventListener('scroll', () => {this.#activeNav()});
-		window.addEventListener('resize', () => {this.#activeNav()});
-		window.addEventListener('orientationchange', () => {this.#activeNav()});
+		this.activeClass = options?.activeClass;
+		this.duration = options?.duration ?? 500;
+		this.easing = options?.easing ?? 'ease';
+		this.helmet = this.setHelmet(options?.helmet);
+		this.iosNotch = options?.iosNotch ? this.notchValue : 0;
+		this.transOffset = this.setTransOffset(options?.transOffset);
+
+		this.init(options);
 	}
 
+	/**
+	 * iOS notch 값 조회
+	 */
 	get notchValue(){
 		const tempEle = document.createElement('div');
 		tempEle.setAttribute('style', 'top:constant(safe-area-inset-top);top:env(safe-area-inset-top)');
@@ -30,9 +31,12 @@ class SmartNav{
 		return value;
 	}
 
+	/**
+	 * 이동할 영역들의 ID 값 조회
+	 */
 	get targetHash(){
 		const hashArr = [];
-	
+
 		this.navBtns.forEach(navBtn => {
 			const id = navBtn.hash.replace('#', '');
 			hashArr.push(id);
@@ -41,49 +45,84 @@ class SmartNav{
 		return hashArr;
 	}
 
-	#activeNav(){
-		if(this.activeClass){
-			const targets = [];
-	
-			this.targetHash.forEach(target => {
-				targets.push(
-					document.getElementById(target)
-				);
-			});
-	
-			targets.forEach(target => {
-				if(target){
-					const rect = {
-						top: target.getBoundingClientRect().top - this.fixed - this.transPoint,
-						bottom: target.getBoundingClientRect().bottom - this.fixed - this.transPoint
-					};
-		
-					if(rect.top <= 0 && rect.bottom > 0){
-						const targetId = target.getAttribute('id');
-						const activeLink = document.querySelector(`${this.wrapper} a[href="#${targetId}"]`);
-						const inactiveLinks = document.querySelectorAll(`${this.wrapper} a:not([href="#${targetId}"])`);
-		
-						inactiveLinks?.forEach(inactiveLink => {
-							if(inactiveLink.classList.contains(this.activeClass)) inactiveLink.classList.remove(this.activeClass);
-						});
-		
-						if(!activeLink?.classList.contains(this.activeClass)) activeLink?.classList.add(this.activeClass);
-					}
-				}
-			});
+	/**
+	 * 헤더, 네비 높이 값만큼 간격 적용
+	 * @param {object} helmet - offsetHeight를 반환할 HTML 요소
+	 * @returns {number}
+	 */
+	setHelmet(helmet){
+		if(helmet ?? typeof helmet == 'object'){
+			const value = helmet.offsetHeight;
+			this.helmet = value;
+			return value;
+		}else{
+			this.helmet = 0;
+			return 0;
 		}
 	}
 
-	#smoothScroll(target){
+	setTransOffset(transOffset){
+		if(transOffset){
+			const value = transOffset * window.innerHeight;
+			this.transOffset = value;
+			return value;
+		}else{
+			this.transOffset = 0;
+			return 0;
+		}
+	}
+
+	/**
+	 * 네비 버튼 on/off class 구분
+	 */
+	setActiveClass(){
+		const targets = [];
+
+		this.targetHash.forEach(target => {
+			targets.push(
+				document.getElementById(target)
+			);
+		});
+
+		targets.forEach(target => {
+			if(target){
+				const rect = {
+					top: target.getBoundingClientRect().top - this.helmet - this.transOffset,
+					bottom: target.getBoundingClientRect().bottom - this.helmet - this.transOffset
+				};
+	
+				if(rect.top <= 0 && rect.bottom > 0){
+					const targetId = target.getAttribute('id');
+					const activeLink = document.querySelector(`${this.wrapper} a[href="#${targetId}"]`);
+					const inActiveLinks = document.querySelectorAll(`${this.wrapper} a:not([href="#${targetId}"])`);
+	
+					inActiveLinks?.forEach(inActiveLinks => {
+						if(inActiveLinks.classList.contains(this.activeClass)){
+							inActiveLinks.classList.remove(this.activeClass);
+						}
+					});
+	
+					if(!activeLink?.classList.contains(this.activeClass)){
+						activeLink?.classList.add(this.activeClass);
+					}
+				}
+			}
+		});
+	}
+
+	/**
+	 * 스크롤 이동 애니메이션
+	 * @param {string} target - 이동할 요소의 ID 값
+	 */
+	moveTo(target){
 		const targetEle = document.querySelector(target);
 	
 		if(targetEle){
-			const helmet = this.fixed - this.iosNotch;
+			let startTime = null;
+			const helmet = this.helmet - this.iosNotch;
 			const targetPosition = targetEle.getBoundingClientRect().top;
 			const startPosition = window.scrollY;
 			const distance = targetPosition - helmet;
-			let startTime = null;
-
 			const easingEffect = {
 				linear(t){
 					return t;
@@ -98,7 +137,6 @@ class SmartNav{
 					return 1 - (1 - t) ** 3;
 				}
 			};
-
 			const moveScroll = currentTime => {
 				if(startTime == null) startTime = currentTime;
 
@@ -108,16 +146,13 @@ class SmartNav{
 
 				window.scrollTo(0, currentPosition);
 
-				switch(true){
-					case elapsedTime <= this.duration:
-						requestAnimationFrame(moveScroll);
-						break;
-					case currentTime >= elapsedTime:
-						const value = window.scrollY + targetEle.getBoundingClientRect().top - helmet;
-						window.scrollTo(0, value);
-						break;
-					default:
-						break;
+				if(elapsedTime <= this.duration){
+					// 애니메이션 종료 전
+					requestAnimationFrame(moveScroll);
+				}else if(currentTime >= elapsedTime){
+					// 애니메이션 종료 후
+					const value = window.scrollY + targetEle.getBoundingClientRect().top - helmet;
+					window.scrollTo(0, value);
 				}
 			};
 
@@ -125,14 +160,35 @@ class SmartNav{
 		}
 	}
 
-	#init(){
+	/**
+	 * 초기화
+	 */
+	init(options){
 		this.navBtns.forEach(navBtn => {
-			navBtn.addEventListener('click', event => {
-				event.preventDefault();
-				this.#smoothScroll(event.target.hash);
+			navBtn.addEventListener('click', e => {
+				e.preventDefault();
+				this.moveTo(e.target.hash);
 			});
 		});
 
-		this.#activeNav();
+		// on/off class 구분 있는 경우
+		if(this.activeClass){
+			this.setActiveClass();
+
+			['scroll', 'resize', 'orientationchange'].forEach(event => {
+				window.addEventListener(event, () => {this.setActiveClass()});
+			});
+		}
+
+		// 헤더, 네비 높이 값만큼 간격 적용 있는 경우
+		if(this.helmet){
+			['resize', 'orientationchange'].forEach(event => {
+				window.addEventListener(event, () => {this.setHelmet(options?.helmet)});
+			});
+		}
+
+		['load', 'resize', 'orientationchange'].forEach(event => {
+			window.addEventListener(event, () => {this.setTransOffset(options?.transOffset)});
+		});
 	}
 }
